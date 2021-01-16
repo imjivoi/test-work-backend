ca1894a6f71d6954a40825a53ced092f2c6599a5
imjivoi <viat001@mail.ru>
2021-01-16 18:11:03 -0300

    add procfile,setting for deply

--- back/settings.py
+++ back/settings.py
@@ -13,17 +13,20 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from django.utils import timezone
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+django_heroku.settings(locals())
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aad&xz-o7sjgtt@0k5n+bbzo=j^$$k5%-u)-bv8nqi*2-u7+(e'
+SECRET_KEY = 'aad&xz-o7sjgtt@0k5n+bbzo=j^$$k5%-udds)-bv8nqi*2-u7+(e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -47,15 +50,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-MIDDLEWARE = ['corsheaders.middleware.CorsMiddleware',
-              'django.middleware.security.SecurityMiddleware',
-              'django.contrib.sessions.middleware.SessionMiddleware',
-              'django.middleware.common.CommonMiddleware',
-              'django.middleware.csrf.CsrfViewMiddleware',
-              'django.contrib.auth.middleware.AuthenticationMiddleware',
-              'django.contrib.messages.middleware.MessageMiddleware',
-              'django.middleware.clickjacking.XFrameOptionsMiddleware',
-              ]
+MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 ROOT_URLCONF = 'back.urls'
 
